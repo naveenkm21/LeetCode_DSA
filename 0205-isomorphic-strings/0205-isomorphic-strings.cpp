481c8a7f7c5960f0f2827db07f3671087fde88ac
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-06-23 17:54:51 +0530

    Time: 0 ms (100%), Space: 9.4 MB (56.72%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        int s_map[256] = {0};
+        int t_map[256] = {0};
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (s_map[s[i]] != t_map[t[i]])
+                return false;
+            s_map[s[i]] = i + 2;
+            t_map[t[i]] = i + 2;
+        }
+
+        return true;
+    }
+};
