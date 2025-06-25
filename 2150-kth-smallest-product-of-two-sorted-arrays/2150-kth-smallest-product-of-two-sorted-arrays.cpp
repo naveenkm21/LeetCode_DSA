dba1402078e716d6a64e411f0874cd2ed2d455bb
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-06-25 14:11:13 +0530

    Time: 629 ms (19.94%), Space: 98.1 MB (70.09%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    long long countPairs(vector<int>& nums1, vector<int>& nums2, long long x) {
-        int m = nums2.size();
+    long long countLessOrEqual(const vector<int>& nums1, const vector<int>& nums2, long long x) {
         long long count = 0;
+        int m = nums2.size();
 
         for (int a : nums1) {
             if (a > 0) {
+                // nums2[j] <= x / a
                 int l = 0, r = m - 1;
                 while (l <= r) {
-                    int mid = (l + r) / 2;
+                    int mid = l + (r - l) / 2;
                     if ((long long)a * nums2[mid] <= x)
                         l = mid + 1;
                     else
@@ -16,9 +17,10 @@ public:
                 }
                 count += l;
             } else if (a < 0) {
+                // nums2[j] >= ceil(x / a)
                 int l = 0, r = m - 1;
                 while (l <= r) {
-                    int mid = (l + r) / 2;
+                    int mid = l + (r - l) / 2;
                     if ((long long)a * nums2[mid] <= x)
                         r = mid - 1;
                     else
@@ -26,17 +28,21 @@ public:
                 }
                 count += m - l;
             } else {
-                if (x >= 0) count += m;
+                if (x >= 0)
+                    count += m;
             }
         }
         return count;
     }
 
     long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
         long long left = -1e10, right = 1e10;
         while (left < right) {
             long long mid = left + (right - left) / 2;
-            if (countPairs(nums1, nums2, mid) < k)
+            if (countLessOrEqual(nums1, nums2, mid) < k)
                 left = mid + 1;
             else
                 right = mid;
