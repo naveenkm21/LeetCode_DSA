997755ad118044a079c0042b285096abf2dec301
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-06-26 16:39:46 +0530

    Time: 4 ms (18.96%), Space: 21.7 MB (46.16%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.emplace_back(newInterval);
+        return merge(intervals);
+    }
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> mergedIntervals;
+        mergedIntervals.emplace_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (mergedIntervals.back()[1] < intervals[i][0]) {
+                mergedIntervals.emplace_back(intervals[i]);
+            } else {
+                mergedIntervals.back()[1] = max(mergedIntervals.back()[1], intervals[i][1]);
+            }
+        }
+
+        return mergedIntervals;
+    }
+};
