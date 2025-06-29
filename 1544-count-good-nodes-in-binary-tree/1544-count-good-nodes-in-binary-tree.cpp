2715053f3b20e9a2e76202fb8ca2f332359218e6
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-06-29 18:05:58 +0530

    Time: 104 ms (35.36%), Space: 88.4 MB (28.31%) - LeetHub

---
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return dfs(root, INT_MIN);
+    }
+
+private:
+    int dfs(TreeNode* node, int maxSoFar) {
+        if (!node) return 0;
+
+        int count = 0;
+        if (node->val >= maxSoFar) {
+            count = 1;
+        }
+
+        maxSoFar = max(maxSoFar, node->val);
+        count += dfs(node->left, maxSoFar);
+        count += dfs(node->right, maxSoFar);
+        return count;
+    }
+};
