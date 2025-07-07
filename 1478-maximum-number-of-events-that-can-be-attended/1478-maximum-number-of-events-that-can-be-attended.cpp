ad2279a1621072e4a25e0bfb203bafdfbb4bd1a7
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-07-07 18:28:06 +0530

    Time: 67 ms (38.92%), Space: 74.9 MB (85.96%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,31 +1,31 @@
 class Solution {
- public:
-  int maxEvents(vector<vector<int>>& events) {
-    int ans = 0;
-    int d = 0;  // the current day
-    int i = 0;  // events' index
-    priority_queue<int, vector<int>, greater<>> minHeap;
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
 
-    ranges::sort(events);
+        int n = events.size();
+        int day = 0, i = 0, ans = 0;
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-    while (!minHeap.empty() || i < events.size()) {
-      // If no events are available to attend today, let time flies to the next
-      // available event.
-      if (minHeap.empty())
-        d = events[i][0];
-      // All the events starting from today are newly available.
-      while (i < events.size() && events[i][0] == d)
-        minHeap.push(events[i++][1]);
-      // Greedily attend the event that'll end the earliest since it has higher
-      // chance can't be attended in the future.
-      minHeap.pop();
-      ++ans;
-      ++d;
-      // Pop the events that can't be attended.
-      while (!minHeap.empty() && minHeap.top() < d)
-        minHeap.pop();
+        while (i < n || !pq.empty()) {
+            if (pq.empty() && i < n)
+                day = events[i][0];
+
+            while (i < n && events[i][0] <= day) {
+                pq.push(events[i][1]);
+                ++i;
+            }
+
+            while (!pq.empty() && pq.top() < day)
+                pq.pop();
+
+            if (!pq.empty()) {
+                pq.pop();
+                ++ans;
+                ++day;
+            }
+        }
+
+        return ans;
     }
-
-    return ans;
-  }
 };
