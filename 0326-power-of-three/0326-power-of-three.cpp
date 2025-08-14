46cda1eb1b56aff228cb4054f287dff5fa82de17
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-08-14 10:21:50 +0530

    Time: 4 ms (47.42%), Space: 9 MB (46.4%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,6 +1,10 @@
 class Solution {
- public:
-  bool isPowerOfThree(int n) {
-    return n > 0 && static_cast<int>(pow(3, 19)) % n == 0;
-  }
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
 };
