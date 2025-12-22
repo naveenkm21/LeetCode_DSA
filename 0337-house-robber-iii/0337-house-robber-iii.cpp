cea1a56a685d426d9d8f556ebfe37f5d8121e641
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2025-12-22 17:11:24 +0530

    Time: 0 ms (100%), Space: 22.5 MB (57.18%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,32 @@
+// LeetCode definition:
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right)
+//         : val(x), left(left), right(right) {}
+// };
+
+class Solution {
+public:
+    // returns {take, skip}
+    pair<int,int> dfs(TreeNode* root) {
+        if (!root) return {0, 0};
+
+        auto left  = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int take = root->val + left.second + right.second;
+        int skip = max(left.first, left.second) +
+                   max(right.first, right.second);
+
+        return {take, skip};
+    }
+
+    int rob(TreeNode* root) {
+        auto res = dfs(root);
+        return max(res.first, res.second);
+    }
+};
