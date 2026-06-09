8eaeeb7d4d78654e3065fd80f2373f9abf87b9be
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2026-06-09 11:44:53 +0530

    Time: 71 ms (67.05%), Space: 173.2 MB (67.86%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,32 +1,35 @@
+#include <unordered_map>
+#include <list>
+
 class LRUCache {
 private:
-    int cap;
-    list<pair<int, int>> lru; 
-    unordered_map<int, list<pair<int, int>>::iterator> cache; 
+    int capacity;
+    std::list<std::pair<int, int>> dll; 
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cache;
 
 public:
-    LRUCache(int capacity) : cap(capacity) {}
-
+    LRUCache(int capacity) : capacity(capacity) {}
+    
     int get(int key) {
-        if (cache.find(key) == cache.end())
-            return -1;
-
-        lru.splice(lru.begin(), lru, cache[key]);
+        if (cache.find(key) == cache.end()) return -1;
+        
+        dll.splice(dll.begin(), dll, cache[key]);
         return cache[key]->second;
     }
-
+    
     void put(int key, int value) {
         if (cache.find(key) != cache.end()) {
             cache[key]->second = value;
-            lru.splice(lru.begin(), lru, cache[key]);
-        } else {
-            if (lru.size() == cap) {
-                int lru_key = lru.back().first;
-                lru.pop_back();
-                cache.erase(lru_key);
-            }
-            lru.emplace_front(key, value);
-            cache[key] = lru.begin();
+            dll.splice(dll.begin(), dll, cache[key]);
+            return;
         }
+        
+        if (cache.size() == capacity) {
+            cache.erase(dll.back().first);
+            dll.pop_back();
+        }
+        
+        dll.emplace_front(key, value);
+        cache[key] = dll.begin();
     }
 };
