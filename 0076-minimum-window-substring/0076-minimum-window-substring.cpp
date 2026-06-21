e97efbb6748a9d65cd787e8e507cec14348189a9
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2026-06-22 00:32:02 +0530

    Time: 0 ms (100%), Space: 11.4 MB (64.24%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,37 @@
 class Solution {
- public:
-  string minWindow(string s, string t) {
-    vector<int> count(128);
-    int required = t.length();
-    int bestLeft = -1;
-    int minLength = s.length() + 1;
+public:
+    string minWindow(string s, string t) {
 
-    for (const char c : t)
-      ++count[c];
+        vector<int> freq(128, 0);
 
-    for (int l = 0, r = 0; r < s.length(); ++r) {
-      if (--count[s[r]] >= 0)
-        --required;
-      while (required == 0) {
-        if (r - l + 1 < minLength) {
-          bestLeft = l;
-          minLength = r - l + 1;
+        int missing = t.size();
+
+        int start = -1;
+        int minLen = INT_MAX;
+
+        for(char c : t)
+            freq[c]++;
+
+        for(int left = 0, right = 0; right < s.size(); right++) {
+
+            if(--freq[s[right]] >= 0)
+                missing--;
+
+            while(missing == 0) {
+
+                if(right - left + 1 < minLen) {
+
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                if(++freq[s[left++]] > 0)
+                    missing++;
+            }
         }
-        if (++count[s[l++]] > 0)
-          ++required;
-      }
-    }
 
-    return bestLeft == -1 ? "" : s.substr(bestLeft, minLength);
-  }
+        return start == -1
+               ? ""
+               : s.substr(start, minLen);
+    }
 };
