e00b87bdd11299e5a88bce6174d3524a25a9ad49
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2026-06-22 11:07:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
-        for (char ch : s) {
-            if (ch == '(' || ch == '[' || ch == '{') {
-                stk.push(ch);
-            } else {
-                if (stk.empty()) return false;
-                char top = stk.top(); stk.pop();
-                if ((ch == ')' && top != '(') ||
-                    (ch == ']' && top != '[') ||
-                    (ch == '}' && top != '{')) {
-                    return false;
-                }
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if(st.empty())
+                   return false;
+
+                if(ch == ')' && st.top()!= '(')
+                   return false;
+                if(ch == '}' && st.top()!= '{')
+                   return false;
+                if(ch == ']' && st.top()!= '[')
+                   return false; 
+                st.pop();
             }
         }
-        return stk.empty();
+        return st.empty();
     }
 };
