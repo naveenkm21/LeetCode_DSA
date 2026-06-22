423fc360ed7bb2c81a74ddae8369373806bd0fbc
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2026-06-22 11:49:08 +0530

    Time: 1 ms (49.13%), Space: 17.2 MB (40.52%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        vector<int> st;  
 
-        for (const string& token : tokens) {
-            if (token.size() == 1 && string("+-*/").find(token) != string::npos) {
-                int b = st.back(); st.pop_back();
-                int a = st.back(); st.pop_back();
-                switch (token[0]) {
-                    case '+': st.push_back(a + b); break;
-                    case '-': st.push_back(a - b); break;
-                    case '*': st.push_back(a * b); break;
-                    case '/': st.push_back(a / b); break;
-                }
-            } else {
-                st.push_back(stoi(token));
+        stack<int> st ;
+
+        for(string token: tokens){
+            if(token == "+" || token == "-" || token == "*" || token == "/"){
+                int b = st.top();
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+
+                if(token == "+")
+                  st.push(a+b);
+                else if(token == "-")
+                  st.push(a-b);
+                else if(token == "*")
+                  st.push(a*b);
+                else
+                  st.push(a/b);
+            }
+            else{
+                st.push(stoi(token));
             }
         }
-
-        return st.back();
+        return st.top();
     }
 };
