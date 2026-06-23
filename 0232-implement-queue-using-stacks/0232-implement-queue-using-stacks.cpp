0c2b96a0f3b896c6508562da4219cc43c287ca0b
Naveen Kumar Mohanarajan <naveenkumarmohanrajan@outlook.com>
2026-06-23 10:18:56 +0530

    Time: 0 ms (100%), Space: 10.2 MB (22.08%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,49 +1,47 @@
 class MyQueue {
-
-private: 
-    std::stack<int> stk_in;
-    std::stack<int> stk_out;
-
-    void shiftStacks() {
-        if (stk_out.empty()) {
-            while(!stk_in.empty()) {
-                stk_out.push(stk_in.top());
-                stk_in.pop();
-            }
-        }
-    }
-
 public:
+    
+    stack<int> in;
+    stack<int> out;
+
     MyQueue() {
         
     }
-    
+
     void push(int x) {
-        stk_in.push(x);
+        in.push(x);
     }
-    
+
     int pop() {
-        shiftStacks();
-        int frontElement = stk_out.top();
-        stk_out.pop();
-        return frontElement;
+
+        if(out.empty()) {
+
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        int val = out.top();
+        out.pop();
+
+        return val;
     }
-    
+
     int peek() {
-        shiftStacks();
-        return stk_out.top();
+
+        if(out.empty()) {
+
+            while(!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        return out.top();
     }
-    
+
     bool empty() {
-        return stk_in.empty() && stk_out.empty();
+        return in.empty() && out.empty();
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
